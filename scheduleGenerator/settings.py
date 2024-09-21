493ad98a0367386abae7acd709b9470e9938cff7
Olaf 3 <olaflewan@gmail.com>
2024-09-21 11:01:07 +0200

    static files troubleshooting

--- scheduleGenerator/settings.py
+++ scheduleGenerator/settings.py
@@ -136,6 +136,10 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'generatorApp/static/')]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
