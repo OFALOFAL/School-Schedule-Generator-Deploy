7d6293cce81e3ad1786ca4c5eb26365586bbb043
Olaf 3 <olaflewan@gmail.com>
2024-09-21 13:35:20 +0200

    postgre connect

--- scheduleGenerator/settings.py
+++ scheduleGenerator/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-
 from pathlib import Path
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,10 +92,7 @@ WSGI_APPLICATION = "scheduleGenerator.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config()
 }
 
 
