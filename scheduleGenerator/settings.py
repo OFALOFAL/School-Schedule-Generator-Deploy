4ef1e03db2235bcb53bdbf2436b6dd92871ab5ba
Olaf 3 <olaflewan@gmail.com>
2024-09-21 10:42:18 +0200

    added whitenoise into apps

--- scheduleGenerator/settings.py
+++ scheduleGenerator/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "generatorApp.apps.GeneratorAppConfig",
     "django.contrib.admin",
     "django.contrib.auth",
